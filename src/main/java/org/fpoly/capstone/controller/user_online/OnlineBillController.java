364cc5dacb4357ba3842ebc23ba5725bcb929369
Tran Xuan Duy <118041118+jamezxje@users.noreply.github.com>
2025-03-24 09:13:20 +0700

    view bill detail

--- src/main/java/org/fpoly/capstone/controller/user_online/OnlineBillController.java
+++ src/main/java/org/fpoly/capstone/controller/user_online/OnlineBillController.java
@@ -1,21 +1,25 @@
 package org.fpoly.capstone.controller.user_online;
 
 import lombok.RequiredArgsConstructor;
+import org.fpoly.capstone.controller.payload.bill_detail.BillDetailViewModel;
 import org.fpoly.capstone.controller.payload.cart_detail.CartDetailViewModel;
 import org.fpoly.capstone.entity.Address;
 import org.fpoly.capstone.entity.Bill;
 import org.fpoly.capstone.entity.User;
 import org.fpoly.capstone.repository.CartRepository;
+import org.fpoly.capstone.service.BillDetailService;
 import org.fpoly.capstone.service.BillService;
 import org.fpoly.capstone.service.CartDetailService;
 import org.fpoly.capstone.service.OnlineAddressService;
 import org.fpoly.capstone.service.UserService;
 import org.fpoly.capstone.service.payload.bill.CreateBillRequest;
+import org.fpoly.capstone.service.payload.bill_detail.BillDetailResponse;
 import org.fpoly.capstone.service.payload.cart_detail.CartDetailResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -27,6 +31,7 @@ public class OnlineBillController {
 
     private final CartDetailService cartDetailService;
     private final BillService billService;
+    private final BillDetailService billDetailService;
     private final CartRepository cartRepository;
     private final ModelMapper modelMapper;
     private final UserService userService;
@@ -73,5 +78,21 @@ public class OnlineBillController {
         List<Bill> billList = this.billService.findBillsByCustomerId(loggedUser.getId());
         model.addAttribute("billList", billList);
         return "/views/user-online-view/bill/bill-management";
+    }
+
+    @GetMapping(path = "{billId}")
+    public String onOpenBillDetail(@PathVariable("billId") Long billId,
+                                   Model model) {
+
+        List<BillDetailResponse> billDetailResponseList =
+                this.billDetailService.findBillDetailByBillId(billId);
+
+        List<BillDetailViewModel> viewModels = billDetailResponseList.stream()
+                .map(response -> this.modelMapper.map(response, BillDetailViewModel.class))
+                .toList();
+
+        model.addAttribute("billDetailResponseList", viewModels);
+
+        return "/views/user-online-view/bill/bill-detail";
     }
 }
--- src/main/java/org/fpoly/capstone/repository/BillDetailRespository.java
+++ src/main/java/org/fpoly/capstone/repository/BillDetailRespository.java
@@ -1,9 +1,27 @@
 package org.fpoly.capstone.repository;
 
 import org.fpoly.capstone.entity.BillDetail;
+import org.fpoly.capstone.service.payload.bill_detail.BillDetailResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BillDetailRespository extends JpaRepository<BillDetail, Long> {
+    @Query("SELECT NEW org.fpoly.capstone.service.payload.bill_detail.BillDetailResponse(" +
+            "bd.id, " +
+            "bd.productDetail.id, " +
+            "bd.productDetail.product.name, " +
+            "bd.productDetail.featureImage, " +
+            "bd.productDetail.size.name, " +
+            "bd.productDetail.color.name, " +
+            "bd.productDetail.price, " +
+            "bd.quantity) " +
+            "FROM BillDetail bd " +
+            "WHERE bd.bill.id = :billId ORDER BY bd.createDate DESC ")
+    List<BillDetailResponse> findBillDetailByBillId(@Param("billId") Long billId);
+
 }
