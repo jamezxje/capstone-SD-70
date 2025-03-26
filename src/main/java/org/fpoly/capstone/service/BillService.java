f88cac28ba0f765e4d78d655619bd03730a2aa50
Tran Xuan Duy <118041118+jamezxje@users.noreply.github.com>
2025-03-26 14:39:50 +0700

    feature buy now for user

--- src/main/java/org/fpoly/capstone/service/BillService.java
+++ src/main/java/org/fpoly/capstone/service/BillService.java
@@ -1,15 +1,21 @@
 package org.fpoly.capstone.service;
 
 import org.fpoly.capstone.entity.Bill;
-import org.fpoly.capstone.entity.Cart;
+import org.fpoly.capstone.service.payload.bill.BuyNowBillRequest;
 import org.fpoly.capstone.service.payload.bill.CreateBillRequest;
 
 import java.util.List;
 
 public interface BillService {
 
-    void saveToBillForOnlineUser(Cart cart, CreateBillRequest request);
+    void saveToBillForOnlineUser(CreateBillRequest request);
 
     List<Bill> findBillsByCustomerId(Long customerId);
+
+    void buyNowForOnlineUser(BuyNowBillRequest request);
+
+    List<Bill> findLastestBillByCustomerId();
+
+    void saveToBillForBuyNow(CreateBillRequest createBillRequest);
 
 }
