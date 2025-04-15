17e3b602c0b871252b05014cf7ec339d16b8cda2
Bảo Phạm <phamthaibao2410@gmail.com>
2025-04-16 06:48:27 +0700

    update

--- src/main/java/org/fpoly/capstone/entity/BillDetail.java
+++ src/main/java/org/fpoly/capstone/entity/BillDetail.java
@@ -1,22 +1,7 @@
 package org.fpoly.capstone.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -38,14 +23,12 @@ public class BillDetail {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "id_product_detail", referencedColumnName = "id")
-    @JsonBackReference
     private ProductDetail productDetail;
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "id_bill", referencedColumnName = "id")
-    @JsonBackReference
     private Bill bill;
 
     @Column(name = "quantity")
