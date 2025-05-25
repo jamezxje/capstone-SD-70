5f9b2b2e5f289c6afca479ba62c834ac8c3c8030
Tran Xuan Duy <118041118+jamezxje@users.noreply.github.com>
2025-05-25 11:05:45 +0700

    update BillDetail entity

--- src/main/java/org/fpoly/capstone/entity/BillDetail.java
+++ src/main/java/org/fpoly/capstone/entity/BillDetail.java
@@ -1,11 +1,9 @@
 package org.fpoly.capstone.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,7 +35,8 @@ public class BillDetail {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    //    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "id_product_detail", referencedColumnName = "id")
     private ProductDetail productDetail;
 
